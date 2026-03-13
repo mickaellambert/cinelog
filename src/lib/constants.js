4ba89f6845293d06e57a234a86b37de7a839ce98
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2026-03-13 23:02:19 +0100

    feat: add shared constants (GENRES, STATUSES)

---
+++ src/lib/constants.js
@@ -0,0 +1,17 @@
+export const GENRES = [
+  'Action',
+  'Animation',
+  'Comedy',
+  'Crime',
+  'Documentary',
+  'Drama',
+  'Horror',
+  'Romance',
+  'Sci-Fi',
+  'Thriller',
+]
+
+export const STATUSES = {
+  to_watch: 'À voir',
+  watched: 'Vu',
+}
